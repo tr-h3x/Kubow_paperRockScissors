4d4cdffdd603f9683b56f97af4c97bbccd4273df
tr_h3x <travis.hetzel@gmail.com>
2022-04-08 16:58:16 -0700

    Follow the first 5-ish minutes, then maybe return

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Kubow_paperRockScissors
 Ania Kubow YT tutorial - follow along
+
+https://www.youtube.com/watch?v=RwFeg0cEZvQ&t=150
+
+Ania Kubow shows three different ways to code PRS
+
+Create html, js, etc.
---
+++ myscript.js
@@ -0,0 +1,6 @@
+const computerChoice = document.getElementById("computer-choice")
+const userChoice = document.getElementById(user-choice)
+const resultDisplay = document.getElementById(result)
+const possibleChoice = document.querySelector("button")
+
+possibleChoices.forEach(possibleChoice)
